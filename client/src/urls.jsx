8396ce62ded93588f0ed5379dec7967b662d8680
Ryan Tauro <23c35.rayan@sjec.ac.in>
2025-11-09 14:41:47 +0530

    even more meh

---
+++ client/src/urls.jsx
@@ -0,0 +1,6 @@
+export const API_URL = {
+  BASE: 'http://localhost:8080/api',
+  AUTH: 'http://localhost:8080/api/auth',
+  COMPLAINT_FORM: 'http://localhost:8080/api/complaints',
+  MY_COMPLAINTS: 'http://localhost:8080/api/complaints/me'
+}
